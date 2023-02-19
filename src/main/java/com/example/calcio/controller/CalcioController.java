2bad38a74eb47d736dbe511df387ff50381cf755
Mykola Danyliuk <danyliuk.m.w@gmail.com>
2023-02-19 16:11:38 +0200

    add crossorigin annotation

--- src/main/java/com/example/calcio/controller/CalcioController.java
+++ src/main/java/com/example/calcio/controller/CalcioController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
--- src/main/java/com/example/calcio/controller/GlobalExceptionHandler.java
+++ src/main/java/com/example/calcio/controller/GlobalExceptionHandler.java
@@ -1,13 +1,11 @@
 package com.example.calcio.controller;
 
-import com.example.calcio.exceptions.NotFoundException;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
